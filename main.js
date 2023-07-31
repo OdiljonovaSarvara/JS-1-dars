97949ed9f0dd49eb6a7e6dd971cfe7cf298870f2
dastgddg11wregds <136077247+OdiljonovaSarvara@users.noreply.github.com>
2023-07-31 15:49:22 +0500

    JS 1-dars
    24-guruh o'quvchisi

---
+++ main.js
@@ -0,0 +1,57 @@
+// console.log(2+2)
+// alert("Sayitimizga hush kelibsiz!")
+// document.write("Men bodiy oynasida ko'rinaman!")
+
+// let x = 2;
+// document.write(x)
+
+// let x = 2;
+// let y = 5;
+// x = 10
+// document.write(x*y)
+
+// let x = 10, y = 5, z = 2;
+// document.write(x+y+z)
+
+/* Data type in JS */
+
+/*let x = 5;*/                               /* number */
+/*let matn = "JavaScrip"*/                 /* string */
+/*document.write(typeof matn)*/
+
+/* Math in JS */
+/* let x = 5; */
+/* x+=1; */
+/* x-=1; */
+/* x*=1 */
+/* x/=1; */
+/* let qoldiq = x%6; */
+/* let daraja = x**3; */
+/* ++x; */
+/* --x; */
+/* --x; */
+/* x++; */
+/* document.write(x+5) */
+
+/* if (agar) */
+/* else (u holda) */
+/* else if (yoki bo'lmasa) */
+
+/* if (45 <= 46){
+    console.log("Ular teng!")
+}
+else{
+    console.log("Ular teng emas!")
+} */
+
+if (45 == 44){
+    console.log("Birinchisidagi qiymatlar teng!")
+}
+
+else if (45== 45){
+    console.log("Ikkinchisidagi qiymatlar teng!")
+}
+
+else{
+    console.log("Uchinchisidagi qiymatlar teng emas!")
+}
